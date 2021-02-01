3dcfc3293293fe24953f00f02e97a0aff9f6eb4f
Arilin Thorferra <arilin.thorferra@gmail.com>
2021-02-01 05:41:35 +0000

    Fix typo

--- src/main.php
+++ src/main.php
@@ -6,30 +6,30 @@
 
 // Set up BASE_DIR global so the application knows where it's installed
 
-define('BASE_DIR', dirname(__FILE__) . '/');
+define("BASE_DIR", dirname(__FILE__) . "/");
 
 // Register a class autoloader
 
 spl_autoload_register(function ($class) {
-    $file = BASE_DIR . str_replace('\\', '/', $class) . '.php';
+    $file = BASE_DIR . str_replace("\\", "/", $class) . ".php";
     require $file;
 });
 
 // Register an exception handler
 
 set_exception_handler(function ($e) {
-    // generate an HTTP 500 (Interal Server Error) response
+    // generate an HTTP 500 (Internal Server Error) response
     $response = new \Http\Response(500);
     http_response_code(500);
     // if debug mode is enabled, add a stack trace
-    if (Settings::get('debug')) {
+    if (Settings::get("debug")) {
         $response->addToBody(
             '<b style="color:#C00">' .
                 $e->getMessage() .
-                '</b>' .
+                "</b>" .
                 '<pre style="font:12px/1.5 Monaco">' .
                 $e->getTraceAsString() .
-                '</pre>'
+                "</pre>"
         );
     }
     echo $response->getBody();
