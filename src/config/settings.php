2d7a8a48bf605c8d63ee9351bc6691ed589d14da
Arilin Thorferra <arilin.thorferra@gmail.com>
2021-01-31 16:08:30 -0800

    Formatting tweaks

--- src/config/settings.php
+++ src/config/settings.php
@@ -4,8 +4,11 @@
  * Configuration settings
  */
 return [
-    'debug' => true,
+    // database setup
     'db_adapter' => 'Sqlite',
-    'dsn' => 'sqlite:' . BASE_DIR . 'db/bfdb.db',
-    'session' => '12a6ee8416a1ef91786416de380c89aa0f2c2a84aea5fcc5',
+    'dsn'        => 'sqlite:' . BASE_DIR . 'db/bfdb.db',
+    // arbitrary session ID
+    'session'    => '12a6ee8416a1ef91786416de380c89aa0f2c2a84aea5fcc5',
+    // debug mode
+    'debug'      => true,
 ];
